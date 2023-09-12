f1d76eec834ee256c89bb662ea8c1d2365ae7183
Felix Adnel <felixadnel@Felixs-MacBook-Pro.local>
2023-09-12 02:28:35 -0500

    initial commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -32,18 +32,33 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - set them up as one of each derived class
              * Set the properties with object initializer syntax
              */
+            Car Scatpack = new Car() { HasTrunk = true, Make = "Dodge", Model = "Scatpack", Year = 2022 };
+            Motorcycle Kawasaki = new Motorcycle { HasSdieCart = true, Make = "kawasaki", Model = "Ninja", Year = 2017 };
 
+            Vehicle sport = new Car() { HasTrunk = true, Make = "Dodge", Model = "Hellcat", Year = 2023 };
+            Vehicle coupe = new Car() { HasTrunk = true, Make = "BMW", Model = "M3", Year = 2021 };
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+            vehicles.Add(Scatpack);
+            vehicles.Add(Kawasaki);
+            vehicles.Add(sport);
+            vehicles.Add(coupe);
 
+            foreach(var veh in vehicles)
+            {
+                Console.WriteLine($"Make {veh.Make} Model {veh.Model} Year {veh.Year}");
+                veh.DriveVirtual();
+                Console.WriteLine("-------------------------------------");
+            }
             // Call each of the drive methods for one car and one motorcycle
 
             #endregion            
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleUI
+{
+	public abstract class Vehicle
+	{
+        public int Year { get; set; } = 1;
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Default Model";
+
+        public abstract void DriveAbstract(); // stubed out method. (no implementation)
+
+        public virtual void DriveVirtual() // Has implementation (scope / stuff it will do)
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive.");
+
+        }
+    }
+}
+
+ 
